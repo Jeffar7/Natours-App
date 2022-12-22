fa6fbb5a9500294df30f3194845fa9af5a1b8016
Jeffar Manurung <jeffarmanurung66@gmail.com>
2022-12-23 00:04:14 +0700

    laptop

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+//  1) MIDDLEWARES
+
+// third party middleware = MORGAN
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log(`Hello from Global the middleware`);
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTES HANDLERS
+const home = (req, res) => {
+  res.status(200).json({
+    status: 'OK'
+  });
+};
+
+// 3) ROUTES
+app.get('/', home);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,26 @@
+// START SERVER
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+

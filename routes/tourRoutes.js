b00c4eb6eaf5b9ef07b99ef9b001965825346ad7
Jeffar <jeffarmanurung66@gmail.com>
2022-12-28 21:31:09 +0700

    158. Implementing Simple Nested Routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const tourController = require(`../controllers/tourController`);
 const router = express.Router();
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 // router.param('id', tourController.checkID);
 
 router
@@ -24,6 +25,14 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;

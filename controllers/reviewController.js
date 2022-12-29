74e2b32e8819c46d10464a456de35a277ea7d570
Jeffar <jeffarmanurung66@gmail.com>
2022-12-29 11:08:33 +0700

    163. Factory Functions: Reading

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,30 +1,8 @@
 const Review = require('./../models/reviewModel');
-const APIFeatures = require('./../utils/apiFeatures');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+// const APIFeatures = require('./../utils/apiFeatures');
+// const catchAsync = require('./../utils/catchAsync');
+// const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
-
-exports.getAllReview = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  if (!reviews) {
-    res.json({
-      message: 'data empty'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-  next();
-});
 
 exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
@@ -33,6 +11,8 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getAllReview = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
